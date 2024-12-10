c44fc918ce4b7debdd69b4cb18383c210ee3dbb8
hasbulla <hasbulla@machine>
2024-12-10 04:15:14 -0300

    Update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     readme = fh.read()
 
 setup(name='LibDOM',
-    version='0.0.7',
+    version='0.0.8',
     url='https://github.com/PSalleSDev/LibDOM',
     license='MIT License',
     author='Pedro Salles',
